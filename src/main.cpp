0d508051b1b3a021163953f5af0fbc1269cf6f38
kikuepi <kikuepi@gmail.com>
2025-11-11 12:45:01 +0900

    processString関数を追加。

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,84 @@
-#include <bits/stdc++.h>
+#include "lib.h"
 using namespace std;
 
 #include "Albat/albat.h"
 #include "Utils/stringutils.h"
 
-int main()
+#ifdef __EMSCRIPTEN__
+#include <emscripten.h>
+extern "C" {
+#endif
+
+#ifdef __EMSCRIPTEN__
+EMSCRIPTEN_KEEPALIVE
+const char* processString(const char* input)
 {
-    string code, str;
-    char buf[10000];
-    while(1)
-    {
-        int siz = fread(buf, 1, 10000, stdin);
-        code += string(buf, siz);
-        if(siz < 10000) break;
-    }
-    str = code;
+    static std::string output;
+
+    string code = input;
     StringUtils::update_Function_main(code);
     Albat albat;
     albat.parse(code, "", 0, LINETYPES::PROGRAM);
+
     std::string lib;
     lib = LibraryManager::getInstance().extractInsertLibraries("head");
     albat.insert(lib, 0);
+
     lib = LibraryManager::getInstance().extractInsertLibraries("header_of_main");
-    for(int i=0;i<albat.lines.size();i++)
+    for(int i=0; i<albat.lines.size(); i++)
     {
-        if(albat.lines[i].substr(0,10) == "int main()")
+        if(albat.lines[i].substr(0, 10) == "int main()")
         {
             albat.nextPtrs[albat.nextindices[i]]->insert(lib, 0);
             break;
         }
     }
+
+    code = '\n'+albat.print(0);
+
+    return code.c_str();
+}
+#endif
+
+int main()
+{
+#ifndef __EMSCRIPTEN__
+    string code, str;
+    char buf[10000];
+    while (1)
+    {
+        int siz = fread(buf, 1, 10000, stdin);
+        code += string(buf, siz);
+        if (siz < 10000)
+            break;
+    }
+    str = code;
+    StringUtils::update_Function_main(code);
+    Albat albat;
+    albat.parse(code, "", 0, LINETYPES::PROGRAM);
+
+    std::string lib;
+    lib = LibraryManager::getInstance().extractInsertLibraries("head");
+    albat.insert(lib, 0);
+    lib = LibraryManager::getInstance().extractInsertLibraries("header_of_main");
+
+    for (int i = 0; i < albat.lines.size(); i++)
+    {
+        if (albat.lines[i].substr(0, 10) == "int main()")
+        {
+            albat.nextPtrs[albat.nextindices[i]]->insert(lib, 0);
+            break;
+        }
+    }
+
     albat.print(0);
     printf("//code\n/*\n");
     printf("%s\n", str.c_str());
     printf("*/\n");
-    // albat.debug(0);
+#endif
     return 0;
 }
+
+#ifdef __EMSCRIPTEN__
+} // extern "C"
+#endif
