d7c9c3490d367edba4ffa584225f299bd73cb9e1
kikuepi <kikuepi@gmail.com>
2025-12-03 15:26:08 +0900

    EmscriptenでコンパイルしてないときもprocessString関数を使ってC++のソースコードを出力するようにした。

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
 #include "lib.h"
-using namespace std;
 
 #include "Albat/albat.h"
 #include "Utils/stringutils.h"
@@ -7,15 +6,16 @@ using namespace std;
 #ifdef __EMSCRIPTEN__
 #include <emscripten.h>
 extern "C" {
+#define KEEP EMSCRIPTEN_KEEPALIVE
+#else
+#define KEEP
 #endif
-
-#ifdef __EMSCRIPTEN__
-EMSCRIPTEN_KEEPALIVE
+KEEP
 const char* processString(const char* input)
 {
     static std::string output;
 
-    string code = input;
+    std::string code = input;
     StringUtils::update_Function_main(code);
     Albat albat;
     albat.parse(code, "", 0, LINETYPES::PROGRAM);
@@ -25,43 +25,6 @@ const char* processString(const char* input)
     albat.insert(lib, 0);
 
     lib = LibraryManager::getInstance().extractInsertLibraries("header_of_main");
-    for(int i=0; i<albat.lines.size(); i++)
-    {
-        if(albat.lines[i].substr(0, 10) == "int main()")
-        {
-            albat.nextPtrs[albat.nextindices[i]]->insert(lib, 0);
-            break;
-        }
-    }
-
-    code = '\n'+albat.print(0);
-
-    return code.c_str();
-}
-#endif
-
-int main()
-{
-#ifndef __EMSCRIPTEN__
-    string code, str;
-    char buf[10000];
-    while (1)
-    {
-        int siz = fread(buf, 1, 10000, stdin);
-        code += string(buf, siz);
-        if (siz < 10000)
-            break;
-    }
-    str = code;
-    StringUtils::update_Function_main(code);
-    Albat albat;
-    albat.parse(code, "", 0, LINETYPES::PROGRAM);
-
-    std::string lib;
-    lib = LibraryManager::getInstance().extractInsertLibraries("head");
-    albat.insert(lib, 0);
-    lib = LibraryManager::getInstance().extractInsertLibraries("header_of_main");
-
     for (int i = 0; i < albat.lines.size(); i++)
     {
         if (albat.lines[i].substr(0, 10) == "int main()")
@@ -71,13 +34,34 @@ int main()
         }
     }
 
-    albat.print(0);
+    output = albat.print(0);
+    return output.c_str();
+}
+
+int main()
+{
+#ifndef __EMSCRIPTEN__
+    std::string code, str;
+    char buf[10000];
+    while (1)
+    {
+        int siz = fread(buf, 1, 10000, stdin);
+        code += std::string(buf, siz);
+        if (siz < 10000)
+            break;
+    }
+    str = code;
+    const char* result = processString(code.c_str());
+    printf("%s\n", result);
+
     printf("//code\n/*\n");
     printf("%s\n", str.c_str());
     printf("*/\n");
 #endif
     return 0;
 }
+
+
 
 #ifdef __EMSCRIPTEN__
 } // extern "C"
